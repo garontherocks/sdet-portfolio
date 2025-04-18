a4f40a41dcde7aa7b8f09ffe9df8d3abfc062549
Victor <garguy@gmail.com>
2025-04-18 18:09:01 -0300

    fix: implemented percy in a specific test

--- playwright/scripts/percy-snapshots.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-import percySnapshot from '@percy/playwright';
-
-test.describe('Percy Visual Snapshots - SauceDemo', () => {
-  test('Home Page Snapshot', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await percySnapshot(page, 'SauceDemo Home');
-  });
-});
